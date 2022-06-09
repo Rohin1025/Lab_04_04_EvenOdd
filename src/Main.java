5d0bfe7a231eb213b4983011768932276139de4a
Rohin <kotianra@mail.uc.edu>
2022-06-09 11:52:23 -0400

    Share Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class
+Main
+{
+    public static void main(String[] args)
+    {
+        int number = 3; // random number used here to depict odd/even
+        int DIVIDEND = 2;
+        int remainder = number % DIVIDEND;
+        if (remainder > 0)
+        {
+            System.out.println(number + " is odd");
+        }
+        else
+        {
+            System.out.println(number + " is even");
+        }
     }
 }
